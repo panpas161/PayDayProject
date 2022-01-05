f68e2efd56efe41300a0c23f817b5707ee113e37
panpas161 <panpas161@gmail.com>
2022-01-05 19:14:43 +0200

    Changes

--- src/view/ViewCardsPopup.java
+++ src/view/ViewCardsPopup.java
@@ -8,6 +8,23 @@ import java.util.ArrayList;
 public class ViewCardsPopup extends JFrame {
     public ViewCardsPopup(ArrayList<DealCard> dealCards)
     {
+        ArrayList<JPanel> cardPanels = new ArrayList<>();
+        for(int i=0;i<dealCards.size();i++)
+        {
+            cardPanels.add(new JPanel());
+            cardPanels.get(i).add(
+                    new JLabel(dealCards.get(i).getTitle())
+            );
+            cardPanels.get(i).add(
+                    new JLabel(
+                        new ImageIcon(dealCards.get(i).getImage())
+                    )
+            );
+        }
+        for(int i=0;i<dealCards.size();i++)
+        {
+            this.add(cardPanels.get(i));
+        }
         this.setSize(400,500);
         this.setLayout(null);
         this.setVisible(true);
